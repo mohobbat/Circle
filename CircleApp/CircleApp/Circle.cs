823a1d1570d74692762c0bbac39d6afe2fa21089
mohobbat <raihan_ice07@yahoo.com>
2014-09-29 15:28:13 +0600

    add

---
+++ CircleApp/CircleApp/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CircleApp
+{
+    class Circle
+    {
+        public double radius;
+
+        public double GetDiameter()
+        {
+            return 2*radius;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2*Math.PI*radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI*radius*radius;
+        }
+    }
+}
---
+++ CircleApp/CircleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle aCircle = new Circle();
+            aCircle.radius = 100;
+            Console.WriteLine("Perimeter: " + aCircle.GetPerimeter());
+            Console.WriteLine("Diameter: " + aCircle.GetDiameter());
+            Console.WriteLine("Area: " + aCircle.GetArea());
+            Console.ReadLine();
+        }
+    }
+}
